7bf31f52f882d785d6fcdf888c4fc99d1452451e
Gennady Dobretsov <dobrets@gmail.com>
2023-02-19 23:48:43 +0600

    Initial Commit

---
+++ sakhaKeyboardApp/sakhaKeyboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sakhaKeyboardAppApp.swift
+//  sakhaKeyboardApp
+//
+//  Created by dobrets on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct sakhaKeyboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
