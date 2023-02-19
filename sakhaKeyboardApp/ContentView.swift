5f7c8ae66786f40c3230b8e279eb6b77e3c39c26
Gennady Dobretsov <dobrets@gmail.com>
2023-02-20 00:34:09 +0600

    App replaced with textField to make debugging easy

--- sakhaKeyboardApp/ContentView.swift
+++ sakhaKeyboardApp/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("placeholder", text: $text)
+                .padding(.top)
+            Spacer()
         }
         .padding()
     }
